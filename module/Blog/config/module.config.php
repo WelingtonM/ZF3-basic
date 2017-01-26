0cd211a8fdd3ef43ab45a4ffdeefce0359e8affa
Welington J. Miyazato <welingtonmiyazato@gmail.com>
2017-01-27 02:04:11 +0900

    01 - Blog Module

---
+++ module/Blog/config/module.config.php
@@ -0,0 +1,23 @@
+<?php
+namespace Blog;
+
+
+
+return [
+
+    'router'=>[
+
+    ],
+
+    'controllers'=>[
+        'factories' => [
+        ],
+    ],
+
+    'view_manager'=>[
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ]
+
+];
