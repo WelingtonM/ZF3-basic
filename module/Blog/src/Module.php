2b3f8adab4aa84cc42185e44dc767e2f3f9e9d56
Welington J. Miyazato <welingtonmiyazato@gmail.com>
2017-01-27 13:56:05 +0900

    06 - Configuring Adapter and tableGateway

--- module/Blog/src/Module.php
+++ module/Blog/src/Module.php
@@ -1,11 +1,34 @@
 <?php
 namespace Blog;
 
-class Module
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+class Module implements ConfigProviderInterface
 {
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Models\PostTable::class => function ($container) {
+                    $tableGateway = $container->get(Models\PostTableGateway::class);
+                    return new Models\PostTable($tableGateway);
+                },
+                Models\PostTableGateway::class => function($container){
+                    $dbAdapater = $container->get(AdapterInterface::class);
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Models\Post());
+                    return new TableGateway('posts', $dbAdapater, null, $resultSetPrototype);
+                }
+            ]
+        ];
     }
 }
 
